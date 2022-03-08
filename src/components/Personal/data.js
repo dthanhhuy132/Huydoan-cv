daa099c3f46993a24f6475cde39cc40631a2e5f6
huydoan <dthanhhuy132@gmail.com>
2022-03-08 14:19:43 +0700

    modify personal

--- src/components/Personal/data.js
+++ src/components/Personal/data.js
@@ -20,7 +20,7 @@ const personalData = {
     {
       name: "Ngày năng lượng thế giới",
       introduction:
-        "Hưởng ứng ngày ''Năng lượng thế giới'' với nhiều hoạt động, trong đó với các poster nhằm nâng cao nhật thức và góp phần tuyên truyền",
+        "Hưởng ứng ngày ''Năng lượng thế giới'' với nhiều hoạt động, trong đó với các poster nhằm nâng cao nhật thức và góp phần tuyên truyền các hành động tiết kiệm năng lượng và bảo vệ môi trường",
       imgList: importAll(
         require.context(
           "../../assets/activityImg/energyDay/",
@@ -43,13 +43,52 @@ const personalData = {
       ),
     },
   ],
+
+  en: [
+    {
+      name: "Energy Day",
+      introduction:
+        "For Responding to ''World Energy Day'' with many activities, including posters to raise awareness and contribute to propaganda about actions to save energy and protect the environment",
+      imgList: importAll(
+        require.context(
+          "../../assets/activityImg/energyDay/",
+          true,
+          /\.(png|jpg|svg)$/
+        )
+      ),
+    },
+
+    {
+      name: "Environement Day",
+      introduction:
+        "This is an environmental education and communication activity at a secondary school in Binh Duong for responding to the ''World Environment Day''",
+      imgList: importAll(
+        require.context(
+          "../../assets/activityImg/environmentDay/",
+          true,
+          /\.(png|jpg|svg)$/
+        )
+      ),
+    },
+  ],
 };
 
 export const personalDataOther = {
   vn: {
     name: "Phân loại rác và hạn chế sử dụng chai nhựa",
-    introduction:
-      "Đây là hoạt động giáo dục, truyền thông môi trường tại 1 trường THCS ở Bình Dương, hưởng ứng ngày ''Môi trường thế giới''",
+
+    imgList: importAll(
+      require.context(
+        "../../assets/activityImg/other",
+        true,
+        /\.(png|jpg|svg)$/
+      )
+    ),
+  },
+
+  en: {
+    name: "''Waste separation'' and ''Reduce plastic bottles using''",
+
     imgList: importAll(
       require.context(
         "../../assets/activityImg/other",
--- src/components/Personal/index.jsx
+++ src/components/Personal/index.jsx
@@ -21,13 +21,25 @@ export default function Personal() {
   });
 
   function handleClickLg(e) {
-    console.log(isGrabSlide);
     if (isGrabSlide) e.preventDefault();
   }
 
   return (
     <div className="personal">
-      {personalData["vn"].map((item, i) => (
+      <p
+        className="personal__introduction"
+        style={{ fontWeight: 500, marginBottom: "25px" }}
+      >
+        <q>
+          <i>
+            {context.language === "vn"
+              ? "Bên dưới là các thiết kế do em thực hiện khi tham gia vào các hoạt động trước đây. Đó cũng là một phần lý do khiến em rẽ sang con đường lập trình web. Cảm ơn anh/chị đã xem!!!"
+              : "The pictures as below are my design when participating in previous activities. That is also part of the reason why I turned to web programming. Thank you for watching"}
+          </i>
+        </q>
+      </p>
+
+      {personalData[context.language].map((item, i) => (
         <div key={i}>
           <div className="personal__introduction">
             <h2>{item.name}</h2>
@@ -53,13 +65,12 @@ export default function Personal() {
 
       <div>
         <div className="personal__introduction">
-          <h2>{personalDataOther["vn"].name}</h2>
-          <p>{personalDataOther["vn"].introduction}</p>
+          <h2>{personalDataOther[context.language].name}</h2>
         </div>
 
         <div className="lightGallery-wapper-other ">
           <Fancybox>
-            {personalDataOther["vn"]?.imgList.map((img, i) => (
+            {personalDataOther[context.language]?.imgList.map((img, i) => (
               <a
                 data-fancybox="gallery"
                 href={img}
